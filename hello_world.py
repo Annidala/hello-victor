05529cb40299c52b70b52282c24b1831cbb686e8
François Bari <francois.bari@gmail.com>
2017-06-09 14:27:58 +0200

    PREMIER COMMIT

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+langue = input("Choose YOUR LANGUAGE")
+Hellos = {
+    "français": "Bonjour Monde !",
+    "english": "Bitch wanna say HELLO",
+    "deutsch": "Hallo World!"}
+
+print(Hellos[langue.lower()])
